c48171b58038863e70e3c5d23cd30fbbc2a5e598
PrismoidNW <prismoid@cimeyclust.com>
2022-11-18 15:01:09 -0800

    - Made the createTables method which takes  var args of Table - Added MySQL Connector dependency - Added Java doc comments for a detailed description of methods TODO:   - Table mapping/Table models to use an object as the table so that it auto updates in real time   - Other database implementations Imma go on vacation to celebrate Thanksgiving

---
+++ StarDatabase/src/main/java/dev/galactic/star/database/impl/StarDatabase.java
@@ -0,0 +1,98 @@
+package dev.galactic.star.database.impl;
+
+import dev.galactic.star.database.impl.objects.Table;
+import dev.galactic.star.database.mysql.MySqlDatabase;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * @author PrismoidNW
+ */
+public abstract class StarDatabase {
+
+    private Connection connection;
+
+    /**
+     * The type of supported database at the moment. It takes an enum with several values.
+     *
+     * @param dbType Returns a new instance of the database corresponding with the type set. It inherits The
+     *               StarDatabase class.
+     * @return StarDatabase
+     */
+    public static StarDatabase type(DatabaseType dbType) {
+        if (dbType == DatabaseType.MySQL) {
+            return new MySqlDatabase();
+        }
+        try {
+            throw new Exception(
+                    "Please report this to the developers by opening an issue. Error: forgot to add case " +
+                            "statement" +
+                            " in switch.");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @return boolean(true / false) whether the connection is not null AND not closed already.
+     * @throws SQLException The exception thrown by the isClosed method of the Java Connection class.
+     */
+    public boolean isOpen() throws SQLException {
+        return connection != null && !connection.isClosed();
+    }
+
+    /**
+     * The abstract method that is called after calling the type method.
+     *
+     * @param username     The username to the login of the database.
+     * @param password     The password to the login of the database. Recommended to have a strong, long password.
+     * @param host         The url, domain, or IP of the database itself.
+     * @param tableName    The name that will identify the table it is to change.
+     * @param port         The port that goes along with the host url when connecting.
+     * @param extraQueries The extra queries one may want to add.
+     * @return A new instance of the class corresponding with the type you set in the beginning.
+     */
+    public abstract StarDatabase connect(String username, String password, String host, String tableName, int port,
+                                         String extraQueries);
+
+    /**
+     * The abstract method that is called after connecting to create a database.
+     *
+     * @param tables the list of tables to create
+     * @throws SQLException if you don't have any tables to create
+     */
+    public abstract void createTables(Table... tables) throws SQLException;
+
+    /**
+     * Returns the connection of the database.
+     *
+     * @return Connection
+     */
+    public Connection getConnection() {
+        return connection;
+    }
+
+    /**
+     * This is a protected method that sets the connections for child classes.
+     *
+     * @param connection The connection to set.
+     */
+    protected void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    /**
+     * Disconnects the connection if not already disconnected.
+     *
+     * @throws SQLException when either it is null or already closed.
+     */
+    public void disconnect() throws SQLException {
+        if (connection == null) {
+            throw new SQLException("Connection is \"null\".");
+        } else if (!connection.isClosed()) {
+            throw new SQLException("Connection is already closed.");
+        }
+        connection.close();
+    }
+}
---
+++ StarDatabase/src/main/java/dev/galactic/star/database/mysql/MySqlDatabase.java
@@ -0,0 +1,60 @@
+package dev.galactic.star.database.mysql;
+
+import dev.galactic.star.database.impl.StarDatabase;
+import dev.galactic.star.database.impl.objects.Column;
+import dev.galactic.star.database.impl.objects.Table;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Map.Entry;
+
+/**
+ * @author PrismoidNW
+ */
+public class MySqlDatabase extends StarDatabase {
+    @Override
+    public StarDatabase connect(String username, String password, String host, String tableName, int port,
+                                String extraQueries) {
+        try {
+            String connectQuery = "jdbc:mysql://" + host + ":" + port + "/" + tableName + (extraQueries.isEmpty() ?
+                    "" : "?" + extraQueries);
+            this.setConnection(DriverManager.getConnection(connectQuery, username, password));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return this;
+    }
+
+    @Override
+    public void createTables(Table... tables) throws SQLException {
+        if (tables.length == 0) {
+            throw new SQLException("You can't have a varargs with nothing in it.");
+        } else {
+            for (Table table : tables) {
+                StringBuilder mainSb = new StringBuilder("CREATE TABLE IF NOT EXISTS " + table.getName() + "(");
+                for (Entry<String, Column> cl : table.getColumns().entrySet()) {
+                    Column column = cl.getValue();
+                    String key = cl.getKey();
+                    mainSb.append(key)
+                            .append(" ")
+                            .append(column.getFieldType().name())
+                            .append("(")
+                            .append(column.getMaxSize())
+                            .append(")")
+                            .append(!column.canBeNull() ? " NOT NULL" : "")
+                            .append(column.autoIncrements() ? " AUTO_INCREMENT" : "")
+                            .append(", ");
+                }
+                boolean isEmpty = table.getPrimaryKey().isEmpty();
+                if (isEmpty) {
+                    mainSb.delete(mainSb.length() - 2, mainSb.length());
+                }
+                mainSb.append(!isEmpty ? "PRIMARY KEY(" + table.getPrimaryKey() + "));" : ");");
+                PreparedStatement ps = this.getConnection().prepareStatement(mainSb.toString());
+                ps.execute();
+                ps.close();
+            }
+        }
+    }
+}
