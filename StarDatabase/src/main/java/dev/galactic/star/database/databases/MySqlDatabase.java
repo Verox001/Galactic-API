5df700af01bf1b2ddf07f6c305597ae7008912bf
Verox001 <altergeist987@gmail.com>
2022-12-04 14:00:19 +0100

    Merged files

--- StarDatabase/src/main/java/dev/galactic/star/database/databases/MySqlDatabase.java
+++ StarDatabase/src/main/java/dev/galactic/star/database/databases/MySqlDatabase.java
@@ -1,4 +1,21 @@
-package dev.galactic.star.database.mysql;
+
+/*
+ * Copyright 2022-2022 Galactic Star Studios
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.galactic.star.database.databases;
 
 import dev.galactic.star.database.impl.StarDatabase;
 import dev.galactic.star.database.impl.mapping.annotations.DatabaseField;
@@ -14,6 +31,7 @@ import java.sql.SQLException;
 import java.util.Map.Entry;
 
 /**
+ * The class that implements MySQL's own syntax.
  * @author PrismoidNW
  */
 public class MySqlDatabase extends StarDatabase {
