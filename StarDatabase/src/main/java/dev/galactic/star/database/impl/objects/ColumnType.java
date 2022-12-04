78f621c23242e83541e872839ec5b01fd6d60807
Verox001 <altergeist987@gmail.com>
2022-12-04 13:42:59 +0100

    Added Database Reflection with Annotation and Field reflection.

--- StarDatabase/src/main/java/dev/galactic/star/database/impl/objects/ColumnType.java
+++ StarDatabase/src/main/java/dev/galactic/star/database/impl/objects/ColumnType.java
@@ -1,25 +1,42 @@
 package dev.galactic.star.database.impl.objects;
 
+import sun.jvm.hotspot.utilities.Bits;
+
+import java.sql.Time;
+import java.sql.Date;
+
 public enum ColumnType {
-    INT("INT"),
-    FLOAT("FLOAT"),
-    BIT("BIT"),
-    BOOL("BOOL"),
-    DATE("DATE"),
-    TIME("TIME"),
-    DATE_TIME("DATETIME"),
-    CHAR("CHAR"),
-    VARCHAR("VARCHAR"),
-    TEXT("TEXT"),
-    BINARY("BINARY");
+    INT("INT", Integer.class, 11),
+    FLOAT("FLOAT", Float.class, 0),
+    BIT("BIT", Bits.class, 0),
+    BOOL("BOOL", Boolean.class, 0),
+    DATE("DATE", java.util.Date.class, 0),
+    TIME("TIME", Time.class, 0),
+    DATE_TIME("DATETIME", Date.class, 0),
+    CHAR("CHAR", Character.class, 1),
+    VARCHAR("VARCHAR", String.class, 255),
+    TEXT("TEXT", String.class, 65535),
+    BINARY("BINARY", Byte.class, 0);
 
     private final String name;
+    private final Class<?> type;
+    private final int defaultLength;
 
-    ColumnType(String name) {
+    ColumnType(String name, Class<?> type, int defaultLength) {
         this.name = name;
+        this.type = type;
+        this.defaultLength = defaultLength;
     }
 
     public String getName() {
         return name;
+    }
+
+    public Class<?> getType() {
+        return type;
+    }
+
+    public int getDefaultLength() {
+        return defaultLength;
     }
 }
