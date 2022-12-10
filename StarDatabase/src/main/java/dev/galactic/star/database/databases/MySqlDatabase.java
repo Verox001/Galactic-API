2e50e12ae631d6516c72e839d88722a95ac6d0e0
Verox001 <altergeist987@gmail.com>
2022-12-10 20:25:03 +0100

    Wrote UnitTests and fixed potential bugs.

--- StarDatabase/src/main/java/dev/galactic/star/database/databases/MySqlDatabase.java
+++ StarDatabase/src/main/java/dev/galactic/star/database/databases/MySqlDatabase.java
@@ -18,14 +18,17 @@
 package dev.galactic.star.database.databases;
 
 import dev.galactic.star.database.impl.StarDatabase;
+import dev.galactic.star.database.impl.mapping.annotations.DatabaseField;
 import dev.galactic.star.database.impl.mapping.annotations.DatabaseTable;
 import dev.galactic.star.database.impl.objects.Column;
 import dev.galactic.star.database.impl.objects.Table;
 
 import java.io.InvalidClassException;
+import java.lang.reflect.Field;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.Map.Entry;
 
 /**
@@ -34,10 +37,10 @@ import java.util.Map.Entry;
  */
 public class MySqlDatabase extends StarDatabase {
     @Override
-    public StarDatabase connect(String username, String password, String host, String tableName, int port,
-                                String extraQueries) {
+    public StarDatabase connect(String username, String password, String host, String database, int port,
+                                String extraQueries) throws RuntimeException {
         try {
-            String connectQuery = "jdbc:mysql://" + host + ":" + port + "/" + tableName + (extraQueries.isEmpty() ?
+            String connectQuery = "jdbc:mysql://" + host + ":" + port + "/" + database + (extraQueries.isEmpty() ?
                     "" : "?" + extraQueries);
             this.setConnection(DriverManager.getConnection(connectQuery, username, password));
         } catch (SQLException e) {
@@ -79,44 +82,57 @@ public class MySqlDatabase extends StarDatabase {
     }
 
     @Override
-    public void createTables(Class<?>... tables) throws SQLException {
+    public void createTables(Class<?>... tables) throws SQLException, InvalidClassException {
         if (tables.length == 0) {
             throw new SQLException("You can't have a varargs with nothing in it.");
         } else {
             for (Class<?> table : tables) {
-                StringBuilder mainSb = new StringBuilder("CREATE TABLE IF NOT EXISTS " + table.getName() + "(");
+                StringBuilder query = generateCreationString(table);
 
-                /*for (Entry<String, Column> cl : table.getColumns().entrySet()) {
-                    Column column = cl.getValue();
-                    String key = cl.getKey();
-                    mainSb.append(key)
-                            .append(" ")
-                            .append(column.getFieldType().name())
-                            .append("(")
-                            .append(column.getMaxSize())
-                            .append(")")
-                            .append(!column.canBeNull() ? " NOT NULL" : "")
-                            .append(column.autoIncrements() ? " AUTO_INCREMENT" : "")
-                            .append(", ");
-                }
-                boolean isEmpty = table.getPrimaryKey().isEmpty();
-                if (isEmpty) {
-                    mainSb.delete(mainSb.length() - 2, mainSb.length());
-                }
-                mainSb.append(!isEmpty ? "PRIMARY KEY(" + table.getPrimaryKey() + "));" : ");");*/
-                PreparedStatement ps = this.getConnection().prepareStatement(mainSb.toString());
+                boolean isEmpty = table.getAnnotation(DatabaseTable.class).primaryKeyField().equals("");
+                if (isEmpty && table.getDeclaredFields().length > 0) query.delete(query.length() - 2, query.length());
+                query.append(!isEmpty ? "PRIMARY KEY(" + table.getAnnotation(DatabaseTable.class).primaryKeyField() + "));" : ");");
+                System.out.println(query);
+                PreparedStatement ps = this.getConnection().prepareStatement(query.toString());
                 ps.execute();
                 ps.close();
             }
         }
     }
 
-    private void generateCreationString(Class<?> table) throws InvalidClassException {
+    private StringBuilder generateCreationString(Class<?> table) throws InvalidClassException {
         if (!table.isAnnotationPresent(DatabaseTable.class)) {
             throw new InvalidClassException(
                     "The class " + table.getName() + " doesn't annotate from DatabaseTable. " +
                             "Read more and see how to fix this here: "
             );
         }
+
+        String tableName = table.getSimpleName();
+        if (!table.getAnnotation(DatabaseTable.class).tableName().equals("")) {
+            tableName = table.getAnnotation(DatabaseTable.class).tableName();
+        }
+        StringBuilder query = new StringBuilder("CREATE TABLE IF NOT EXISTS " + tableName + " (");
+        for (Field column : table.getDeclaredFields()) {
+            if (column.isAnnotationPresent(DatabaseField.class)) {
+                String name = column.getAnnotation(DatabaseField.class).name();
+                if (name.equals("")) name = column.getName().toLowerCase();
+                int maxSize = column.getAnnotation(DatabaseField.class).maxSize();
+                if (maxSize > column.getAnnotation(DatabaseField.class).fieldType().getDefaultLength()) {
+                    maxSize = column.getAnnotation(DatabaseField.class).fieldType().getDefaultLength();
+                }
+                query
+                        .append(name)
+                        .append(" ")
+                        .append(column.getAnnotation(DatabaseField.class).fieldType().getName())
+                        .append("(")
+                        .append(maxSize)
+                        .append(")")
+                        .append(!column.getAnnotation(DatabaseField.class).canBeNull() ? " NOT NULL" : "")
+                        .append(column.getAnnotation(DatabaseField.class).autoIncrements() ? " AUTO_INCREMENT" : "")
+                        .append(", ");
+            }
+        }
+        return query;
     }
 }
